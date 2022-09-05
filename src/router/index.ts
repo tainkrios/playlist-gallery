6e7fb30c6e8ebfe578197fb839d1c6597ff2cdeb
Ivan Zhdanov <tain.krios@gmail.com>
2022-09-06 00:40:07 +0200

    feat: add Signup component.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/auth/Login.vue'
+import Signup from '../views/auth/Signup.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: Signup
     }
   ]
 })
---
+++ src/views/auth/Signup.vue
@@ -0,0 +1,32 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <h3>Sign up</h3>
+    <input type="text" placeholder="Display name" v-model="displayName">
+    <input type="email" placeholder="Email" v-model="email">
+    <input type="password" placeholder="Password" v-model="password">
+    <div class="error" v-if="error">{{ error }}</div>
+    <button v-if="!isPending">Sign up</button>
+    <button v-if="isPending" disabled>Loading...</button>
+  </form>
+</template>
+
+<script setup lang="ts">
+import { ref } from '@vue/reactivity';
+import { useSignup } from '../../composables/useSignup';
+
+const { error, signup, isPending } = useSignup(),
+      email                        = ref(''),
+      password                     = ref(''),
+      displayName                  = ref(''),
+      handleSubmit = async () => {
+        await signup(email.value, password.value, displayName.value)
+        if (!error.value) {
+          console.log('user signed up')
+        }
+      }
+
+</script>
+
+<style scoped>
+
+</style>
